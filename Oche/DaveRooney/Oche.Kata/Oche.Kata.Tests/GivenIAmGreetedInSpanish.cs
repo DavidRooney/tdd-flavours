9dcf2aaec903bf308818909db2e11da46a413cc2
David Rooney <david_rooney16@hotmail.com>
2022-08-21 21:04:15 +0100

    Green - Simplify test and make it pass

--- Oche/DaveRooney/Oche.Kata/Oche.Kata.Tests/GivenIAmGreetedInSpanish.cs
+++ Oche/DaveRooney/Oche.Kata/Oche.Kata.Tests/GivenIAmGreetedInSpanish.cs
@@ -3,21 +3,26 @@ namespace Oche.Kata.Tests
     public class GivenIAmGreetedInSpanish
     {
         [Test]
-        public void WhenItIsTheEveningOrEarlyMorning_ThenNoches()
+        public void WhenItIsTheEvening_ThenNoches()
         {
             var eveningTime = new TimeSpan(20, 00, 00);
 
             var sut = new Greeter();
             var result = sut.Execute(eveningTime);
 
-            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.EqualTo("¡Buenas noches"));
         }
 
         public class Greeter
         {
-            public object Execute(TimeSpan eveningTime)
+            public string Execute(TimeSpan eveningTime)
             {
-                throw new NotImplementedException();
+                if (eveningTime.Hours >= 20)
+                {
+                    return "¡Buenas noches";
+                }
+
+                return "FAIL";
             }
         }
     }
