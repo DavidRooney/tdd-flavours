d07799a2e81772557b071ac8b5fd1d3a8194c4c6
David Rooney <david_rooney16@hotmail.com>
2022-08-21 21:06:18 +0100

    Red - Return name in greeting message

--- Oche/DaveRooney/Oche.Kata/Oche.Kata.Tests/GivenIAmGreetedInSpanish.cs
+++ Oche/DaveRooney/Oche.Kata/Oche.Kata.Tests/GivenIAmGreetedInSpanish.cs
@@ -3,23 +3,23 @@ namespace Oche.Kata.Tests
     public class GivenIAmGreetedInSpanish
     {
         [Test]
-        public void WhenItIsTheEvening_ThenNoches()
+        public void WhenItIsTheEvening_ThenNoches_WithUsername()
         {
-            var eveningTime = new TimeSpan(20, 00, 00);
+            var eveningTime = new TimeSpan(20, 0, 0);
 
             var sut = new Greeter();
-            var result = sut.Execute(eveningTime);
+            var result = sut.Execute(eveningTime, "Dave");
 
             Assert.That(result, Is.EqualTo("¡Buenas noches"));
         }
 
         [Test]
-        public void WhenItIsTheEarlyMorning_ThenNoches()
+        public void WhenItIsTheEarlyMorning_ThenNoches_WithUsername()
         {
             var eveningTime = new TimeSpan(3, 0, 0);
 
             var sut = new Greeter();
-            var result = sut.Execute(eveningTime);
+            var result = sut.Execute(eveningTime, "Dave");
 
             Assert.That(result, Is.EqualTo("¡Buenas noches"));
         }
