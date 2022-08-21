a575f3e91f78f6e43b870346cb8b78313df3da14
David Rooney <david_rooney16@hotmail.com>
2022-08-21 21:37:40 +0100

    Console app

---
+++ Oche/DaveRooney/Oche.Kata/Oche.Kata.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Oche.Kata;
+
+Console.WriteLine("Whats your name?");
+var usersName = Console.ReadLine();
+
+Console.WriteLine(new Greeter().Execute(DateTime.Now.Hour, usersName));
+
+while (true)
+{
+    Console.WriteLine("What text do you want to reverse?");
+    var inputText = Console.ReadLine();
+
+    if (inputText.ToLowerInvariant() == "stop")
+    {
+        Environment.Exit(1);
+    }
+
+    var reversedText = new Reverser().Execute(inputText);
+    Console.WriteLine(reversedText);
+
+}
