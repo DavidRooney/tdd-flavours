0d1271f4d3f2684811a001bf29123d41377e6ae9
David Rooney <david_rooney16@hotmail.com>
2022-08-21 21:27:32 +0100

    Refactor - Move working class into class library

--- Oche/DaveRooney/Oche.Kata/Oche.Kata.Tests/GivenIAmGreetedInSpanish.cs
+++ Oche/DaveRooney/Oche.Kata/Oche.Kata.Tests/GivenIAmGreetedInSpanish.cs
@@ -47,32 +47,4 @@ namespace Oche.Kata.Tests
             Assert.That(result, Is.EqualTo($"¡Buenas tardes {_username}"));
         }
     }
-
-    public class Greeter
-    {
-        public string Execute(int hour, string username)
-        {
-            if (hour > 23)
-            {
-                throw new ApplicationException("Hour cannot be more than 23");
-            }
-
-            if (hour >= 20 || hour < 6)
-            {
-                return $"¡Buenas noches {username}";
-            }
-
-            if (hour >= 6 && hour < 12)
-            {
-                return $"¡Buenas dias {username}";
-            }
-
-            if (hour >= 12 && hour < 20)
-            {
-                return $"¡Buenas tardes {username}";
-            }
-
-            return "Not set yet";
-        }
-    }
 }
---
+++ Oche/DaveRooney/Oche.Kata/Oche.Kata/Greeter.cs
@@ -0,0 +1,30 @@
+﻿namespace Oche.Kata
+{
+    public class Greeter
+    {
+        public string Execute(int hour, string username)
+        {
+            if (hour > 23)
+            {
+                throw new ApplicationException("Hour cannot be more than 23");
+            }
+
+            if (hour >= 20 || hour < 6)
+            {
+                return $"¡Buenas noches {username}";
+            }
+
+            if (hour >= 6 && hour < 12)
+            {
+                return $"¡Buenas dias {username}";
+            }
+
+            if (hour >= 12 && hour < 20)
+            {
+                return $"¡Buenas tardes {username}";
+            }
+
+            return "Not set yet";
+        }
+    }
+}
